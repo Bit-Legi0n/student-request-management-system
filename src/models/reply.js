9c1eae971c5427396aa39569a5d8c9448637e659
Dilusha <dilushamadushan@gmail.com>
2021-02-01 20:56:56 +0530

    Add reply model

--- src/models/reply.js
+++ src/models/reply.js
@@ -1,1 +1,9 @@
-// Reply class
+class Reply{
+
+    constructor(id ,requestId ,senderID ,details){
+        this.id = id;
+        this.requestId = requestId;
+        this.senderID = senderID;
+        this.details = details;
+    }
+}
