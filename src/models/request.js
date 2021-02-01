8773c4431cbed837904ae94a5fd72056631366de
Dilusha <dilushamadushan@gmail.com>
2021-02-01 21:13:57 +0530

    Add  models add Request and User models

--- src/models/request.js
+++ src/models/request.js
@@ -1,1 +1,13 @@
-// RequestModel class
+class RequestModel{
+
+    constructor(id ,stdID ,stdName ,stdIndex ,staffId ,requestType ,details ,status){
+        this.id = id;
+        this.stdID = stdID;
+        this.stdName = stdName;
+        this.stdIndex = stdIndex;
+        this.staffId = staffId;
+        this.requestType = requestType;
+        this.details = details;
+        this.status = status;
+    }
+}
--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,16 @@
-// User class
+class User{
+
+    constructor(id, username, isStaff, passwordHash){
+        this.id = id;
+        this.username = username;
+        this.isStaff = isStaff;
+        this.passwordHash = passwordHash;
+    }
+
+    
+    validPassword(passwordToTest){
+        
+        
+    }
+    
+}
