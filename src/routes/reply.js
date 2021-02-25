2e71a8be5c6c9b3d0b9c7ffdb6464347e116ec93
nimantha <nimanthacooray@gmail.com>
2021-02-25 08:12:56 +0530

    Change rel paths in routes/reply.js to abs paths
    Changed all the rel paths into abs paths and formatted using prettier

--- src/routes/reply.js
+++ src/routes/reply.js
@@ -1,57 +1,58 @@
 import express from "express";
 import { authChecker } from "../util/middleware";
-import { saveReply, getUser, setRequestStatus, getRequest } from "../util/database";
+import {
+  saveReply,
+  getUser,
+  setRequestStatus,
+  getRequest,
+} from "../util/database";
 import Reply from "../models/reply";
 
 const handler = (pool) => {
-    const replyRouter = express.Router();
+  const replyRouter = express.Router();
 
-    replyRouter.post("/:requestId", authChecker, async (req, res) => {
-        const reqId = req.params.requestId;
-        const userId = req.session.user.userId;
-        const { id, req_id, user_id, datetime, body } = req.body;
-        const reply = Reply(id, req_id, user_id, datetime, body);
+  replyRouter.post("/:requestId", authChecker, async (req, res) => {
+    const reqId = req.params.requestId;
+    const userId = req.session.user.userId;
+    const { id, req_id, user_id, datetime, body } = req.body;
+    const reply = Reply(id, req_id, user_id, datetime, body);
 
-        
-        const request = await getRequest(pool, reqId);
-        // Check if there exists a req with the given id
-        if (request) {
-            //  Check whether the user is saving a reply related to a request submitted by him
-            if (userId == request.student_id || userId == request.staff_id) {
-                await saveReply(pool, reply);
-                res.redirect("../request/" + reqId);
-            }
-            else {
-                res.redirect("../dashboard");
-            }
-        } else {
-            res.redirect("../dashboard");
-        }
-    });
+    const request = await getRequest(pool, reqId);
+    // Check if there exists a req with the given id
+    if (request) {
+      //  Check whether the user is saving a reply related to a request submitted by him
+      if (userId == request.student_id || userId == request.staff_id) {
+        await saveReply(pool, reply);
+        res.redirect("/request/" + reqId);
+      } else {
+        res.redirect("/dashboard");
+      }
+    } else {
+      res.redirect("/dashboard");
+    }
+  });
 
-    replyRouter.post("/:requestId/status", authChecker, async (req, res) => {
-        // Update status of request
-        // Check whether the corresponding staff member
-        const reqId = req.params.requestId;
-        const userId = req.session.user.userId;
+  replyRouter.post("/:requestId/status", authChecker, async (req, res) => {
+    // Update status of request
+    // Check whether the corresponding staff member
+    const reqId = req.params.requestId;
+    const userId = req.session.user.userId;
 
-        const user = await getUser(pool, userId);
-        if (user.type == 'staff') {
-            const request = await getRequest(pool, reqId);
-            if (request && request.staff_id == userId) {
-                await setRequestStatus(pool, reqId, req.body.reqStatus);
-                res.redirect("../request/" + reqId);
-            }
-            else {
-                res.redirect("../dashboard");
-            }
-        }
-        else {
-            res.redirect("../dashboard");
-        }
-    });
+    const user = await getUser(pool, userId);
+    if (user.type == "staff") {
+      const request = await getRequest(pool, reqId);
+      if (request && request.staff_id == userId) {
+        await setRequestStatus(pool, reqId, req.body.reqStatus);
+        res.redirect("/request/" + reqId);
+      } else {
+        res.redirect("/dashboard");
+      }
+    } else {
+      res.redirect("/dashboard");
+    }
+  });
 
-    return replyRouter;
+  return replyRouter;
 };
 
 export default handler;
