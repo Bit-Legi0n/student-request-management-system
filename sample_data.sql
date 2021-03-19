f9f6209e54eafe1e4723fae9bc4c4dff4fd472e0
NethumL <nethumpinsara@gmail.com>
2021-03-19 18:18:21 +0530

    Fix minor errors
    * Fixed error in request type
    * Fixed error in sample data

--- sample_data.sql
+++ sample_data.sql
@@ -65,7 +65,7 @@ VALUES (
         "190564N",
         "190093T",
         now(),
-        "denied",
+        "declined",
         "OTHER",
         "Dear Sir, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
     ),
